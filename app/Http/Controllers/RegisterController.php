8f2a06fb1ffe78a7d644919e4a4a73dcee8c4e44
joaryneto <joary@ectecnologia.com.br>
2022-07-30 11:28:18 -0400

    Update RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -35,9 +35,7 @@ class RegisterController extends Controller
         return redirect('/')->with('success', "Account successfully registered.");*/
 
         $data = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
             'numero_telefone' => ['required', 'numeric', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
         /* Get credentials from .env */
@@ -47,10 +45,11 @@ class RegisterController extends Controller
         $twilio = new Client($twilio_sid, $token);
         $twilio->verify->v2->services($twilio_verify_sid)
             ->verifications
+            //->create('', "sms");
             ->create($data['numero_telefone'], "sms");
 
-        User::create($data);
+        User::create($request->validated());
 
-        return redirect()->route('verify')->with(['numero_telefone' => $data['phone_number']]);
+        return redirect()->route('verify')->with(['numero_telefone' => $data['numero_telefone']]);
     }
 }
