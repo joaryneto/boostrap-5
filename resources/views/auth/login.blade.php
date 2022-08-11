f3455724c02613d5efb6dba6a08f504a359fc767
joaryneto <joary@ectecnologia.com.br>
2022-08-11 18:37:49 -0400

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,15 +29,14 @@
                         <!-- tabs content start here -->
                         <div class="tab-content">
                             <div class="tab-pane active" id="signin" role="tabpanel">
-                                <form method="post" action="{{ route('login.perform') }}" autocomplete="no">
+                                <form autocomplete="false" method="post" action="{{ route('login.perform') }}">
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                                     <div class="login-input-content">
                                     <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text"><i class="material-icons">person</i></span>
                                             </div>
-                                            <input type="username" class="form-control" placeholder="Email ou Usuario" name="username" value="{{ old('username') }}" autocomplete="no" aria-label="Username" style="display:none">
-                                            <input type="username" class="form-control" placeholder="Email ou Usuario" name="username" value="{{ old('username') }}" autocomplete="no" aria-label="Username" required="required" autofocus>
+                                            <input autocomplete="false" type="username" class="form-control" placeholder="Email ou Usuario" name="username" value="{{ old('username') }}" aria-label="Username" required="required" autofocus>
                                             @if ($errors->has('username'))
                                                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
                                             @endif
@@ -46,8 +45,7 @@
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text"><i class="material-icons">lock</i></span>
                                             </div>
-                                            <input type="password" class="form-control" placeholder="Senha" name="password" value="{{ old('password') }}" autocomplete="no" aria-label="password" style="display:none">
-                                            <input type="password" class="form-control" placeholder="Senha" name="password" value="{{ old('password') }}" autocomplete="no" aria-label="password" required="required">
+                                            <input type="password" class="form-control" placeholder="Senha" name="password" value="{{ old('password') }}" aria-label="password" required="required">
                                             @if ($errors->has('password'))
                                                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
                                             @endif
