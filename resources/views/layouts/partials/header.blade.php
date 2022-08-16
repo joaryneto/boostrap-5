25d0b0ffecf739af807d61089aa5f892bfd3deb4
joaryneto <joary@ectecnologia.com.br>
2022-08-16 08:47:24 -0400

    Update header.blade.php

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -72,7 +72,7 @@
       @font-face {
         font-family: 'password';
         font-style: normal;      
-        src: url(css/password.ttf);
+        src: url(https://adv.quer.app/pastor/public/css/password.ttf);
       }
       #password {
         font-family: 'password';
